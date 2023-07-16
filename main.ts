353fa56ed0898cb2496dd82bdda6347c8bb6dd9d
Ben Preston <bpreston@bltsmrt.com>
2023-07-16 01:03:11 -0400

    Listening to Modify instead of Create

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, TAbstractFile, TFile } from 'obsidian';
 import { AutoCanvasSettingsTab, AutoCanvasPluginSettings, DEFAULT_SETTINGS, FolderCanvasMap } from "./settings";
 const fs = require('fs');
 var path = require('path');
+
+
+interface node {
+	id: string;
+	x: number;
+	y: number;
+	width: number;
+	height: number;
+	type: 'file';
+	file: string;
+	color?: string;
+  }
+
 
 export default class AutoCanvasPlugin extends Plugin {
 	settings: AutoCanvasPluginSettings;
@@ -13,98 +26,90 @@ export default class AutoCanvasPlugin extends Plugin {
 		this.addSettingTab(new AutoCanvasSettingsTab(this.app, this));
 
 		this.registerEvent(
-			this.app.vault.on("create", (page) => {
+			this.app.vault.on("modify", async (page) => {
 
-				let targetCanvas = this.tryAddMatch(page);
-				if (targetCanvas)
-					this.openTargetCanvas(targetCanvas)
+				let targetCanvas = this.findFolderCanvasMap(page);
+				if (!targetCanvas)
+					return;
+
+				let addedPage = await this.tryAddPageToCanvas(page, targetCanvas);
+				if (!addedPage)
+					return;
+
+				if (targetCanvas && this.settings.openTargetCanvasOnNewItem)
+					this.openTargetCanvas(targetCanvas);
 			})
 		)
 	}
 
 
-	tryAddMatch(page) {
-
+	findFolderCanvasMap(page: TAbstractFile) : string|null {
 		const folderPath = page.path.substring(0, page.path.lastIndexOf("/") + 1);
-		for(const [key, val] of Object.entries(this.settings.folderCanvasMaps))
-		{
-			let map = val as FolderCanvasMap;
-			if (map.folder == folderPath)
-			{
-				this.tryAddPageToCanvas(page, map.canvas);
-				return map.canvas;
-			}
-		}
+		let entry =  
+			Object.entries(this.settings.folderCanvasMaps)
+			.find(entry => (entry[1] as FolderCanvasMap).folder == folderPath);
 
-		return null;
+		return entry ? (entry[1] as FolderCanvasMap).canvas : null;
 	}
 
 
-	openTargetCanvas(canvasPath) {
 
-		if (this.settings.openTargetCanvasOnNewItem == false)
-			return;
-
+	async openTargetCanvas(canvasPath : string) {
+		
 		let activeLeaf;
 		try { activeLeaf = this.app.workspace.getLeaf(); }
 		catch { return; }
 		if (!activeLeaf) { return; }
 
-		setTimeout( async () => {
-			try {
-				let pageData = this.app.vault.fileMap[canvasPath];
+		try {
+			let pageData = this.app.vault.fileMap[canvasPath];
 
-				if (!pageData)
-					return;
+			if (!pageData)
+				return;
 
-				await activeLeaf.openFile(pageData);
-				activeLeaf.setEphemeralState({ rename: 'all' });
-			} catch (error)
-			{
-				console.log(error);
-			}
-
-		}, 1500);
+			await activeLeaf.openFile(pageData);
+			activeLeaf.setEphemeralState({ rename: 'all' });
+		} catch (error)
+		{
+			console.log(error);
+		}
 	}
 
 
-	tryAddPageToCanvas(page: any, canvas: string) {
 
+	buildData(path: string) : node {
+		return {
+			"type":"file",
+			"file": path,
+			"id": (Math.floor(Math.random() * Date.now())).toString(),
+			"x": 0,
+			"y": 0,
+			"width": 260,
+			"height": 380
+		}
+	}
+
+
+
+	async tryAddPageToCanvas(page: any, canvasPath: string) : Promise<boolean> {
 		try {
+			let tAbstractCanvas = this.app.vault.getAbstractFileByPath(canvasPath) as TFile;
+			let added = false;
 
-			let newNodeObj = {
-				"type":"file",
-				"file": page.path,
-				"id": Math.random() * Date.now(),
-				"x": 0,
-				"y": 0,
-				"width": 260,
-				"height": 380
-			}
+			await this.app.vault.process(tAbstractCanvas, (data) => {
+				let canvasData = JSON.parse(data);
+				let found = canvasData.nodes.find((n: node) => n.file == page.path);
+				if (found) return data;
 
-			let fullPath = path.join(page.vault.adapter.basePath, canvas);
-			
-			fs.readFile(fullPath, 'utf8', (err, data) => {
-
-				if (err){ console.log(err);}
-				if (!data) return;
-
-				let allObjs = JSON.parse(data);
-				allObjs.nodes.push(newNodeObj);
-				console.log(fullPath);
-				fs.writeFile(fullPath, JSON.stringify(allObjs), (err2) => {
-					if (err2)
-						console.log(err2);
-				})
-
-				return;
+				canvasData.nodes.push(this.buildData(page.path));
+				added = true;
+				return JSON.stringify(canvasData);
 			})
-			
 
-			return;
-
+			return added;
 		} catch (error){
 			console.log(error);
+			return false;
 		}
 	}
 
@@ -113,9 +118,11 @@ export default class AutoCanvasPlugin extends Plugin {
 
 	}
 
+	
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
+
 
 	async saveSettings() {
 		await this.saveData(this.settings);
